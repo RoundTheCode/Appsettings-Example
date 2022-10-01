47d7db5e4420a4e0afdea83c499a2645754937fc
David Grace <davidgrace00@gmail.com>
2022-10-01 15:19:54 +0100

    More classes for part 2

---
+++ RoundTheCode.AppSettings.02ConnectionString/AppSettingsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RoundTheCode.AppSettings.Entities;
+
+namespace RoundTheCode.AppSettings
+{
+    public class AppSettingsDbContext : DbContext
+    {
+        public DbSet<Authentication> Authentication { get; private set; }
+
+        public AppSettingsDbContext() : base()
+        {
+
+        }
+
+        public AppSettingsDbContext(DbContextOptions<AppSettingsDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Authentication>().HasKey(s => s.Id);
+            builder.Entity<Authentication>().ToTable("Authentication");
+        }
+    }
+}
---
+++ RoundTheCode.AppSettings.02ConnectionString/Entities/Authentication.cs
@@ -0,0 +1,16 @@
+﻿namespace RoundTheCode.AppSettings.Entities
+{
+    public class Authentication
+    {
+#nullable disable
+        public int Id { get; private set; }
+
+        public string ClientId { get; private set; }
+
+
+        public string ClientSecret { get; private set; }
+
+        public int Attempts { get; private set; }
+
+    }
+}
