aeda9361b57c43955ae1e9809bd1416297141cd2
David Grace <davidgrace00@gmail.com>
2022-10-07 10:19:47 +0100

    Part 2

--- RoundTheCode.AppSettings.02ConnectionString/Controllers/HomeController.cs
+++ RoundTheCode.AppSettings.02ConnectionString/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoundTheCode.AppSettings.Entities;
 
 namespace RoundTheCode.AppSettings.Controllers
 {
@@ -6,10 +8,17 @@ namespace RoundTheCode.AppSettings.Controllers
     [Route("api/home")]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly AppSettingsDbContext _context;
+
+        public HomeController(AppSettingsDbContext context)
         {
-            return Ok(new { Success = true });
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<Authentication> Index()
+        {
+            return await _context.Authentication.FirstAsync(s => s.Id == 1);
         }
     }
 }
--- RoundTheCode.AppSettings.02ConnectionString/Program.cs
+++ RoundTheCode.AppSettings.02ConnectionString/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RoundTheCode.AppSettings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppSettingsDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppSettingsDbContext"));
+});
 
 var app = builder.Build();
 
