ce2ea8d03e7227577b50953af6bb0b462dc8cb3b
David Grace <davidgrace00@gmail.com>
2022-10-07 09:31:15 +0100

    Added part 1

--- RoundTheCode.AppSettings.01ReadValue/Controllers/HomeController.cs
+++ RoundTheCode.AppSettings.01ReadValue/Controllers/HomeController.cs
@@ -7,10 +7,22 @@ namespace RoundTheCode.AppSettings.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok(new { Success = true });
+            return Ok(new { 
+                ClientId = _configuration.GetValue<string>("Authentication:ClientId"),
+                ClientSecret = _configuration.GetValue<string>("Authentication:ClientSecret"),
+                AllowedHosts = _configuration.GetSection("Authentication:AllowedHosts").Get<string[]>(),
+                Attempts = _configuration.GetValue<int>("Authentication:Attempts"),
+            });
         }
     }
 }
